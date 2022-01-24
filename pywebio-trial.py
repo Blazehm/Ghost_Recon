8e53dcb6448b24323c66993a6d3385d1b0d36253
Hritwik Mukherjee <binary250200@gmail.com>
2022-01-25 04:53:31 +0530

    practice file

--- pywebio-trial.py
+++ pywebio-trial.py
@@ -1,15 +1,18 @@
-import whoisinfo
+import libs.whoisinfo as whoisinfo
+import libs.tracertinfo as trace
 import pywebio
 from pywebio.input import input, FLOAT, TEXT
 from pywebio.output import put_text
 
 def reconserver():
-    #domain = input("give your domain name", type=TEXT)
+    domain = input("give your domain name", type=TEXT)
     """
     text=whoisinfo.who(domain)
     put_text("your info: %s", text)
     """
-    input.checkbox("Recon Options",options=['whois','nslookup','nmap'])
+    t=trace.tracert(domain)
+    put_text("TRACE: %s", t)
+    print(t)
 
 if __name__ == '__main__':
     pywebio.start_server(reconserver, port=80)

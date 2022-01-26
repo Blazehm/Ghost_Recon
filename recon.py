624086ce362954da57374f1a54eb9e4ff2d4dc24
Hritwik Mukherjee <binary250200@gmail.com>
2022-01-26 13:14:23 +0530

    added functionality for builtwith

--- recon.py
+++ recon.py
@@ -1,9 +1,10 @@
 from turtle import color
 import libs.whoisinfo as whois
 import libs.tracertinfo as trace
+import libs.builtwithinfo as builtwith
 import pywebio
 from pywebio.input import input, input_group, checkbox, FLOAT, TEXT
-from pywebio.output import put_text, put_table, put_file, put_loading
+from pywebio.output import put_text, put_table, put_file, put_loading, put_row, put_code
 
 
 def execservice(domain, req_service):
@@ -11,17 +12,23 @@ def execservice(domain, req_service):
         w=whois.who(domain)
         with put_loading(shape='border', color='primary'):
             put_text("WHOIS INFORMATION:\n", w)
-            print("END")
+            print("Whois successful")
 
     if 'traceroute' in req_service:
         t=trace.tracert(domain)
         with put_loading(shape='border', color='primary'):
             put_text("TRACEROUTE INFORMATION:\n", t)
-            print("END")
+            print("Traceroute successful")
 
+    if 'builtwith' in req_service:
+        b=builtwith.bwith(domain)
+        put_text("BUILTWITH INFORMATION:\n")
+        for key in b:
+            put_row([put_code(key),None,put_code(b[key])])        
+        print("Builtwith successful")
 
 def reconserver():
-    servicelist=['whois','traceroute']
+    servicelist=['whois','traceroute','builtwith']
     
     data= input_group(
         "Required Information",
@@ -32,6 +39,7 @@ def reconserver():
     )
 #    print(data['servicedata'])
     domain=data['domain']
+    print(domain)
     req_service=[]
     req_service=data['servicedata']
     execservice(domain, req_service) 
